ce9778176fd2ae260380c9bebe027d9d6a312617
Michael Hanel <michaelsupercode@gmail.com>
2024-04-19 15:04:41 +0200

    opt

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AppRouter from './AppRouter';
+import { DisableReactDevTools, disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools ()
 
 ReactDOM.render(
   <React.StrictMode>
